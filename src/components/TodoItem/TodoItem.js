9c0dcd9121ff34d2c3dc414111cbe2581bf158e9
R-M-G <ryan.gregory@ryanmgregory.com>
2020-10-03 01:14:52 -0500

    created TodoItem component and inported into Todo

---
+++ src/components/TodoItem/TodoItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class TodoItem extends Component {
+  render() {
+    return (
+      <div>
+        <p>This is Hello World from TodoItem</p>
+      </div>
+    );
+  }
+}
+
+export default TodoItem;
