254dfb29880adbb5fbbb3ea96286ed2b82254e83
R-M-G <ryan.gregory@ryanmgregory.com>
2020-10-03 00:52:53 -0500

    created Todos component and sourced into App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import Todos from './components/Todos/Todos';
 
 import './App.css';
 
-
 class App extends Component {
-  render(){
+  render() {
     return (
-      <div><h1>ToDo App</h1></div>
-    )
+      <div className="App">
+        <Todos />
+      </div>
+    );
   }
 }
-
 
 export default App;
