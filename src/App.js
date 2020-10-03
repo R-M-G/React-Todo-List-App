ab1072e2b888be9d979819b00080093c1208052d
R-M-G <ryan.gregory@ryanmgregory.com>
2020-10-03 01:10:47 -0500

    set state inside app with 4 examples and sent to Todos via props

--- src/App.js
+++ src/App.js
@@ -4,10 +4,34 @@ import Todos from './components/Todos/Todos';
 import './App.css';
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Buy milk',
+        completed: true,
+      },
+      {
+        id: 2,
+        title: 'Get gas',
+        completed: false,
+      },
+      {
+        id: 3,
+        title: 'Mow lawn',
+        completed: true,
+      },
+      {
+        id: 4,
+        title: 'Clean out garage',
+        completed: true,
+      },
+    ],
+  };
   render() {
     return (
       <div className="App">
-        <Todos />
+        <Todos todos={this.state.todos} />
       </div>
     );
   }
