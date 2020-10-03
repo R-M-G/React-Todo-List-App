c5c537b3fd67b480faddcc420cc06da566288301
R-M-G <ryan.gregory@ryanmgregory.com>
2020-10-03 03:18:43 -0500

    created AddTodo componenet and styled

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './components/Header/Header';
 import Todos from './components/Todos/Todos';
+import AddTodo from './/components/AddTodo/AddTodo';
 
 import './App.css';
 
@@ -52,12 +53,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <Todos
-          todos={this.state.todos}
-          markComplete={this.markComplete}
-          delTodo={this.delTodo}
-        />
+        <div className="container">
+          <Header />
+          <AddTodo />
+          <Todos
+            todos={this.state.todos}
+            markComplete={this.markComplete}
+            delTodo={this.delTodo}
+          />
+        </div>
       </div>
     );
   }
